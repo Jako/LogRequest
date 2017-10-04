94136889ee1124a9a6033e56a84af0c36cde593d
Thomas Jakobi <thomas.jakobi@partout.info>
2017-10-04 16:12:30 +0200

    Normalize code

--- core/components/logrequest/elements/plugins/logrequest.plugin.php
+++ core/components/logrequest/elements/plugins/logrequest.plugin.php
@@ -9,13 +9,14 @@
  * @var array $scriptProperties
  */
 
+$className = 'LogRequest' . $modx->event->name;
+
 $corePath = $modx->getOption('logrequest.core_path', null, $modx->getOption('core_path') . 'components/logrequest/');
 /** @var LogRequest $logrequest */
 $logrequest = $modx->getService('logrequest', 'LogRequest', $corePath . 'model/logrequest/', array(
     'core_path' => $corePath
 ));
 
-$className = 'LogRequest' . $modx->event->name;
 $modx->loadClass('LogRequestPlugin', $logrequest->getOption('modelPath') . 'logrequest/events/', true, true);
 $modx->loadClass($className, $logrequest->getOption('modelPath') . 'logrequest/events/', true, true);
 if (class_exists($className)) {
