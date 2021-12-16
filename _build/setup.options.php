87d5dd6880c47984d6d50c03d886268c38aef201
Thomas Jakobi <thomas.jakobi@partout.info>
2021-12-16 21:35:07 +0100

    Typo

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -20,7 +20,7 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         $output .= '<h2>Install LogRequest</h2>
 
         <p>Thanks for installing LogRequest. This open source extra was
-        developped by Treehill Studio - MODX development in Münsterland.</p>
+        developed by Treehill Studio - MODX development in Münsterland.</p>
 
         <p>During the installation, we will collect some statistical data (the
         hostname, the IP address, the PHP version and the MODX version of your
@@ -35,7 +35,7 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_UPGRADE:
         $output .= '<h2>Upgrade LogRequest</h2>
 
-        <p>LogRequest will be upgraded. This open source extra was developped by
+        <p>LogRequest will be upgraded. This open source extra was developed by
         Treehill Studio - MODX development in Münsterland.</p>
 
         During the upgrade, we will collect some statistical data (the hostname,
