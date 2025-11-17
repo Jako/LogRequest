9524ec1002553cdfd058a9038c2a8df3f1c51712
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:42:58 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/logrequest/lexicon/es/default.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Default Lexicon Entries for LogRequest
+ *
+ * @package logrequest
+ * @subpackage lexicon
+ */
+$_lang['logrequest'] = '';
+$_lang['logrequest.widget.count'] = '';
+$_lang['logrequest.widget.date'] = '';
+$_lang['logrequest.widget.log'] = '';
+$_lang['logrequest.widget.log_desc'] = '';
+$_lang['logrequest.widget.rank'] = '';
+$_lang['logrequest.widget.rank_desc'] = '';
+$_lang['logrequest.widget.value'] = '';
