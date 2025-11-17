afccfe6e2f60054c84fc81011e65134271cf8acf
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:11 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/logrequest/lexicon/es/setting.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Setting Lexicon Entries for LogRequest
+ *
+ * @package logrequest
+ * @subpackage lexicon
+ */
+$_lang['setting_logrequest.debug'] = '';
+$_lang['setting_logrequest.debug_desc'] = '';
+$_lang['setting_logrequest.trigger'] = '';
+$_lang['setting_logrequest.trigger_desc'] = '';
